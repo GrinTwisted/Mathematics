2651d4ca248cc066c46254987651eae61093f8b2
GrinTwisted <grintwisted@gmail.com>
2018-04-13 21:47:24 +1000

    Added Function : Euclids -     Added To : Basic_Mathematics.py -      Purpose : Returns the Greatest Common Divisor between 2                  numbers using Euclid's Equation -     Run Time : O(log(a + b))
    Bug Fixes to Function : Factors (Mathematics.py)
    - No longer continuously stores results when used in a loop
    - No longer returns an incorrect result for numbers such as
      n = 1 and n = 4

--- Basic_Mathematics.py
+++ Basic_Mathematics.py
@@ -6,7 +6,7 @@
 #                 to basic mathematical calculations
 #
 #   Modified By : GrinTwisted
-# Date Modified : 1:25pm - 11 April 2018
+# Date Modified : 9:40pm - 13 April 2018
 # -------------------------------------------------------------------
 
 # Function : Factors
@@ -17,11 +17,25 @@
 #  Outputs : Returns an ordered set of all factors of n
 # Run Time : O(n log n)
 def factors(n, f, s = set()):
+    s.clear()
     try:
-        for i in range(f, n//f):
+        if n == 1: s.update([1])
+        for i in range(f, n//f+1):
             if n % i == 0:
-                s.update([i, n//i], factors2(n, i+1))
+                s.update([i, n//i], factors(n, i+1))
                 break
     except:
         print("Error: Cannot Divide By 0")
     return sorted(s)
+
+
+# Function : Euclids
+#  Purpose : Finds the greatest common divisor of 2 numbers
+#   Inputs : int a = The number your finding factors for a number
+#            int b = The number your starting from to find a factor
+#  Outputs : Returns the greatest common divisor as an integer
+# Run Time : O(log(a + b))
+def Euclids(a, b):
+    if a < b: a, b = b, a
+    if a % b == 0: return b
+    else: return Euclids(b, a % b)
